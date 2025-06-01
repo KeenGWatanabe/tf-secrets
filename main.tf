ff2a39b6c09e14422da79a8ebccd5aeeff0f60ee
KeenGWatanabe <mckeenweb@gmail.com>
2025-06-01 09:54:32 +0800

    mongo

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-# main.tf
+# give a name to secretsmanager
 resource "aws_secretsmanager_secret" "mongodb_uri" {
   name        = "prod/mongodb_uri"
 }
-
+# set references for ECS to use the secret
 resource "aws_secretsmanager_secret_version" "mongodb_uri" {
   secret_id = aws_secretsmanager_secret.mongodb_uri.id
   secret_string = jsonencode({
-    MONGODB_ATLAS_URI = "mongodb+srv://${var.mongodb_username}:${var.mongodb_password}@${var.mongodb_host}/?retryWrites=true&w=majority&appName=${var.mongodb_database}" # :27017/mydb?authSource=admin"
+    MONGODB_URI = "mongodb+srv://${var.mongodb_username}:${var.mongodb_password}@${var.mongodb_host}/?retryWrites=true&w=majority&appName=${var.mongodb_database}" 
   })
 }
 
